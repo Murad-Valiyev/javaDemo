b257e745d5cf1b4c98877c0fb95e697c556be775
The-V-Murad <127323424+The-V-Murad@users.noreply.github.com>
2023-03-08 19:34:54 +0400

    Update Main.java

--- JavaApplication38/src/main/Main.java
+++ JavaApplication38/src/main/Main.java
@@ -15,6 +15,11 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("Hello murad");
+        System.out.println("Hello murad");
+        System.out.println("Hello murad");
+        System.out.println("Hello murad");
+        System.out.println("Hello murad");
+        
     }
     
 }
